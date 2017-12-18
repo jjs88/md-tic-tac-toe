30671963a76db9f037ff83955fdb25c3395d014a
jjs88 <jjs457@gmail.com>
2017-12-17 23:12:41 -0500

    minor edits

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 (function(global) {
 
 
-    // console.log('start game');
     Game.init();
 
-    //  console.log(Math.floor(Math.random() * 9) + 0) //random number between 0-8
-    // console.log(Math.floor(Math.random() * 3) + 0) 
 
 })(window);
